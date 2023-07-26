0672594ff1d7b5f68cd9b644b3801f3546ceb4ad
Francisco Figueroa <91153878+Frankcisco99@users.noreply.github.com>
2023-07-26 11:42:09 -0400

    Update gemas.py

--- gemas.py
+++ gemas.py
@@ -1,10 +1,9 @@
 import turtle
 import random
-
-#Crear Pantalla
+#Configurar pantalla
 pantalla = turtle.Screen()
 pantalla.title("Videojuego")
-pantalla.bgpic("C:/Users/Lab.Computacion/Desktop/Codigo_Futuro/francisco 3b/videojuego/fondo.png")
+pantalla.bgpic("C:/Users/Lab.Computacion/Desktop/Codigo_Futuro/francisco 4e/videojuego/fondo.png")
 pantalla.setup(width=800,height=600)
 
 #Crear personaje
@@ -13,39 +12,14 @@ tortuga.shape("turtle")
 tortuga.color("green")
 tortuga.penup()
 
-#Crear gemas
-gemas = []
-num_gemas = 6
-for i in range(num_gemas):
-    gema = turtle.Turtle()
-    gema.shape("circle")
-    gema.color("blue")
-    gema.penup()
-    gema.goto(random.randint(-300,300),random.randint(-150,70))
-    gemas.append(gema)
-
-obstaculos = []
-num_obstaculos = 10
-for i in range(num_obstaculos):
-    obstaculo = turtle.Turtle()
-    obstaculo.shape("square")
-    obstaculo.color("red")
-    obstaculo.penup()
-    obstaculo.goto(random.randint(-300,300),random.randint(-150,70))
-    obstaculos.append(obstaculo)
-
 score = 0
 game_over = False
+
 def mover_tortuga():
     global score
     tortuga.forward(20)
-    score = score + 1
-def mover_izq():
-    tortuga.left(30)
-def mover_der():
-    tortuga.right(30)
+    score += 1
 pantalla.onkeypress(mover_tortuga,"Up")
-pantalla.onkeypress(mover_izq,"Left")
-pantalla.onkeypress(mover_der, "Right")
 pantalla.listen()
+
 pantalla.mainloop()
