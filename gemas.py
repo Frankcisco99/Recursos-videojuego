bdb14d7de3a15c214f3e0e20ebe6963208e9fe98
Francisco Figueroa <91153878+Frankcisco99@users.noreply.github.com>
2023-07-27 14:57:30 -0400

    Update gemas.py

--- gemas.py
+++ gemas.py
@@ -3,7 +3,7 @@ import random
 #Configurar pantalla
 pantalla = turtle.Screen()
 pantalla.title("Videojuego")
-pantalla.bgpic("C:/Users/Lab.Computacion/Desktop/Codigo_Futuro/francisco 4e/videojuego/fondo.png")
+pantalla.bgpic("ingresar_ruta")
 pantalla.setup(width=800,height=600)
 
 #Crear personaje
@@ -11,15 +11,4 @@ tortuga = turtle.Turtle()
 tortuga.shape("turtle")
 tortuga.color("green")
 tortuga.penup()
-
-score = 0
-game_over = False
-
-def mover_tortuga():
-    global score
-    tortuga.forward(20)
-    score += 1
-pantalla.onkeypress(mover_tortuga,"Up")
-pantalla.listen()
-
 pantalla.mainloop()
