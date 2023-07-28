5fc4cd642e29616df4f8eb1b6a9a505b99c23745
Francisco Figueroa <91153878+Frankcisco99@users.noreply.github.com>
2023-07-28 11:25:33 -0400

    Update gemas.py

--- gemas.py
+++ gemas.py
@@ -1,14 +1,1 @@
-import turtle
-import random
-#Configurar pantalla
-pantalla = turtle.Screen()
-pantalla.title("Videojuego")
-pantalla.bgpic("ingresar_ruta")
-pantalla.setup(width=800,height=600)
 
-#Crear personaje
-tortuga = turtle.Turtle()
-tortuga.shape("turtle")
-tortuga.color("green")
-tortuga.penup()
-pantalla.mainloop()
